3df3b93519bdd90b490ae8691a2e8ace3db5aa38
sternj <sam@samstern.me>
2021-12-10 20:35:36 -0500

    Added support for austin -f

--- parse_austin.py
+++ parse_austin.py
@@ -2,7 +2,7 @@
 from typing import TextIO
 from collections import defaultdict
 
-def parse_austin(austin_pipe_out: TextIO):
+def parse_austin(austin_pipe_out: TextIO, is_full=False, filename_prefix='bias'):
     function_runtimes = defaultdict(lambda : 0)
     for sample in austin_pipe_out:
         # print(sample)
@@ -13,14 +13,18 @@ def parse_austin(austin_pipe_out: TextIO):
         _, _, frames = rest.partition(";")
         if frames == '':
             continue
-        runtime_cpu = int(metrics)
+        if is_full:
+            runtime = metrics.split(',')[0]
+        else:
+            runtime = metrics
+        runtime_cpu = int(runtime)
         for frame in frames.split(';'):
             filename, fn_name, _ = frame.split(':')
-            if 'bias' in filename and fn_name != '<module>':
+            if filename_prefix in filename and fn_name != '<module>':
                 function_runtimes[fn_name] += runtime_cpu
     return function_runtimes
 
 if __name__ == '__main__':
-    f = open('out.txt', 'r')
-    res = parse_austin(f)
+    f = open('austin.out', 'r')
+    res = parse_austin(f, is_full=False, filename_prefix='test-features')
     print(res)
