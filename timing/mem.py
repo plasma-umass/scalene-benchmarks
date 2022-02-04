79e9a621919965f828d33c175135f281b1c453fa
Sam Stern <sam@samstern.me>
2022-02-04 14:00:36 -0500

    updated mem.py

--- timing/mem.py
+++ timing/mem.py
@@ -2,20 +2,22 @@
 
 import time
 import numpy as np
-from sys import argv
+from sys import argv, platform
 
+
+clock = time.CLOCK_UPTIME_RAW if platform == 'darwin' else time.CLOCK_MONOTONIC
 ITS = 10
 SIZE = 100000
 
 def test_memory(no_touch):
     for i in range(ITS):
-        print(f"=== {i} {time.monotonic_ns()}")
+        print(f"=== {i} {time.clock_gettime_ns(clock)}")
         x0 = np.zeros((1000, 10000)) # alloc
         if not no_touch:
             for it in range(1000):
                 for j in range(10000):
                     x0[it][j] += 1
-    print(f"=== {ITS} {time.monotonic_ns()}")
+    print(f"=== {ITS} {time.clock_gettime_ns(clock)}")
             
 
 if __name__ == '__main__':
